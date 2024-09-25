484b7e6d300aac1bcf49aa3eb659a9a8156cb458
Luca <luca.thierry.2004@hotmail.com>
2024-09-25 16:38:39 +0200

    Login system finished

--- StarterKit/Controllers/LoginController.cs
+++ StarterKit/Controllers/LoginController.cs
@@ -9,53 +9,49 @@ namespace StarterKit.Controllers;
 public class LoginController : Controller
 {
     private readonly ILoginService _loginService;
-    
+    private string AUTH_SESSION_KEY = "admin_login";
 
     public LoginController(ILoginService loginService)
     {
         _loginService = loginService;
     }
 
-    [HttpPost("Login")]
-    public IActionResult Login([FromBody] LoginBody loginBody)
-    {
-        // TODO: Impelement login method
+    [HttpPost("login/admin")]
+    public IActionResult LoginAdmin([FromBody] LoginBody loginBody){
+
         if (string.IsNullOrEmpty(loginBody.Username) || string.IsNullOrEmpty(loginBody.Password)){
             return BadRequest("username and password are a must.");
-        } 
+        }
 
         var loginstatus = _loginService.CheckPassword(loginBody.Username, loginBody.Password);
 
         if (loginstatus == LoginStatus.Success){
-            return Ok("Succesfully logged in.");
-        }
-        return Unauthorized("Incorrect password");
-    }
-
-    [HttpPost("/login/admin")]
-    public IActionResult LoginAdmin([FromForm] string username, [FromForm] string password){
-
-        if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)){
-            return BadRequest("username and password are a must.");
+            HttpContext.Session.SetString(AUTH_SESSION_KEY, loginBody.Username);
+            return Ok("Succesfully logged in as an admin.");
         }
 
-        var loginstatus = _loginService.CheckPassword(username, password);
-
-        if (loginstatus == LoginStatus.IncorrectPassword){
-            return Unauthorized("Incorrect password"); 
-        }
-
-        return Ok("Succesfully logged in as an admin.");
+        return Unauthorized("Incorrect password"); 
         
     }
 
 
     [HttpGet("IsAdminLoggedIn")]
-    public IActionResult IsAdminLoggedIn()   
+    public IActionResult IsAdminLoggedIn()
     {
-        // TODO: This method should return a status 200 OK when logged in, else 403, unauthorized
-        
-        return Unauthorized("You are not logged in");
+        string? adminUserName = HttpContext.Session.GetString(AUTH_SESSION_KEY);
+        if (!string.IsNullOrEmpty(adminUserName))
+        {
+            return Ok(new
+            {
+                isLoggedIn = true,
+                adminUser = adminUserName
+            });
+        }
+        return Unauthorized(new
+        {
+            isLoggedIn = false,
+            adminUser = (string)null!
+        });
     }
 
     [HttpGet("Logout")]
